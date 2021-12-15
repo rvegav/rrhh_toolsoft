b537190e76814b585c169e5f9d7100f651907f03
rvegav <andresvega7373@gmail.com>
2021-12-14 23:36:48 -0300

    se modifican todas las pantallas quitando la opcion de cerrar

--- application/views/estadociviles/edit.php
+++ application/views/estadociviles/edit.php
@@ -43,20 +43,7 @@
 					<h2>
 						Estado Civil  | Editar 
 					</h2>
-					<ul class="nav navbar-right panel_toolbox">
-						<li>
-							<a class="collapse-link">
-								<i class="fa fa-chevron-up">
-								</i>
-							</a>
-						</li>
-						<li>
-							<a class="close-link">
-								<i class="fa fa-close">
-								</i>
-							</a>
-						</li>
-					</ul>
+					
 					<div class="clearfix"></div>
 				</div>
 				<div class="row">
--- application/views/horario/list.php
+++ application/views/horario/list.php
@@ -47,18 +47,6 @@
 					<h2>
 						Listado de Horarios
 					</h2>
-					<ul class="nav navbar-right panel_toolbox">
-						<li>
-							<a class="collapse-link">
-								<i class="fa fa-chevron-up"></i>
-							</a>
-						</li>
-						<li>
-							<a class="close-link">
-								<i class="fa fa-close"></i>
-							</a>
-						</li>
-					</ul>
 					<div class="clearfix">
 					</div>
 				</div>
